378e517b6e82d73b235af30a33eabb20f4e60b06
mcamaya <camiamayar@gmail.com>
2023-10-03 19:35:50 -0500

    fix: :bug: Solucionamos el problema de renderizado

--- frontend/src/components/Clientes.jsx
+++ frontend/src/components/Clientes.jsx
@@ -1,51 +1,47 @@
-import React, { useEffect, useState } from "react";
-import Dashboard from "./Dashboard";
+import React, { useState } from "react";
 import MainTable from "./MainTable";
 
+const allKeys = [
+  {
+    field: "nombre", // field = nombre del campo en la db
+    thead: "Nombre", // thead = nombre que se quiere se muestre en la tabla
+  },
+  {
+    field: "numeroDocumento",
+    thead: "N° Documento",
+  },
+  {
+    field: "celular",
+    thead: "Celular",
+  },
+  {
+    field: "email",
+    thead: "Email",
+  },
+  {
+    field: "direccion",
+    thead: "Dirección",
+  },
+  {
+    field: "tipoDocumento",
+    thead: "Tipo de Documento",
+  },
+];
+const urlApi = "http://localhost:8080/api/v1/clientes";
+
 export default function Clientes() {
-  const urlApi = "http://localhost:8080/api/v1/clientes";
   const [clientes, setClientes] = useState([]);
 
-  const allKeys2 = [
-    {
-      field: "nombre", // field = nombre del campo en la db
-      thead: "Nombre", // thead = nombre que se quiere se muestre en la tabla
-    },
-    {
-      field: "numeroDocumento",
-      thead: "N° Documento",
-    },
-    {
-      field: "celular",
-      thead: "Celular",
-    },
-    {
-      field: "email",
-      thead: "Email",
-    },
-    {
-      field: "direccion",
-      thead: "Dirección",
-    },
-    {
-      field: "tipoDocumento",
-      thead: "Tipo de Documento",
-    },
-  ];
-
-  useEffect(() => {
+  const fetchClientes = () => {
     fetch(urlApi)
-      .then((res) => res.json())
-      .then((data) => setClientes(data))
-      .catch((err) => console.log(err));
-  }, []);
-
-  console.log(clientes);
+    .then((res) => res.json())
+    .then((data) => setClientes(data))
+    .catch((err) => console.log(err));
+  }
 
   return (
     <div>
-      {/* {<MainTable dataKeys={allKeys2} dataAPI={clientes} />} */}
-
+      <MainTable apiData={clientes} objKeys={allKeys} apiFetch={() => fetchClientes()} title={"Clientes"} />
     </div>
   );
 }
--- frontend/src/components/MainTable.jsx
+++ frontend/src/components/MainTable.jsx
@@ -1,29 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Button, Table, TableBody, TableCell } from "semantic-ui-react";
+import './MainTable.css';
 
-export default function MainTable({dataKeys, dataAPI}) {
-  const [data, setData] = useState([]);
-  setData(dataAPI);
+export default function MainTable({objKeys, apiFetch, apiData, title}) {
+
+  useEffect(() => {
+    apiFetch()
+  }, []);
 
   return (
-    <div>
-      {/* <table>
-        <thead>
-          <tr>
-            {dataKeys.map((item) => (
-              <th key={Math.random()}>{item.thead}</th>
+    <div className="table-container">
+      <h2 className="main-title">{title}</h2>
+      <Table singleLine>
+        <Table.Header>
+          <Table.Row className="table-row">
+            {objKeys.map((item) => (
+              <Table.HeaderCell key={Math.random()}>{item.thead}</Table.HeaderCell>
             ))}
-          </tr>
-        </thead>
-        <tbody>
-          {dataAPI.map((registro) => (
-            <tr key={registro._id}>
-              {dataKeys.map((item) => (
-                <td>{item.field}</td>
+          </Table.Row>
+        </Table.Header>
+        <TableBody>
+          {apiData.map((registro) => (
+            <Table.Row className="table-row" key={registro._id}>
+
+              {objKeys.map((item) => (
+                <Table.Cell key={Math.random()}>{registro[item.field]}</Table.Cell>
               ))}
-            </tr>
+              
+            </Table.Row>
           ))}
-        </tbody>
-      </table> */}
+        </TableBody>
+      </Table> 
     </div>
   );
 }
